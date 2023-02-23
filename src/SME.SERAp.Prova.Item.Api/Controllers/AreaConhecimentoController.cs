e12738f19c262811910b9b81e6a6b517ca9a28ad
Fernando Groeler <fernando.groeler@gmail.com>
2023-02-23 16:08:41 -0400

    Ajustada ordenação das áreas de conhecimento.

--- src/SME.SERAp.Prova.Item.Api/Controllers/AreaConhecimentoController.cs
+++ src/SME.SERAp.Prova.Item.Api/Controllers/AreaConhecimentoController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SME.SERAp.Prova.Item.Aplicacao.Interfaces;
-using SME.SERAp.Prova.Item.Dominio.Entities;
 using SME.SERAp.Prova.Item.Infra.Dtos;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -12,8 +11,7 @@ namespace SME.SERAp.Prova.Item.Api.Controllers
     [Route("/api/v1/AreaConhecimento")]
     public class AreaConhecimentoController : ControllerBase
     {
-
-        [HttpGet()]
+        [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<SelectDto>), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         public async Task<IActionResult> ObterAreasConhecimento([FromServices] IObterAreasConhecimentoUseCase obterAreasConhecimento)
