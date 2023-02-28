fba11909169655dfc14c9d402904e14327f1c962
Caíque Siqueira <caique.siqueira@amcom.com.br>
2023-02-28 15:23:29 -0300

    Ajuste script 9 renomear colunas

--- scripts/V9__ALTER_NAME_COLUNS_ITEM.sql
+++ scripts/V9__ALTER_NAME_COLUNS_ITEM.sql
@@ -1,6 +1,32 @@
-ALTER TABLE item 
-RENAME column are_conhecimento_legado_id  TO area_conhecimento_id;
-ALTER TABLE item 
-RENAME column disciplina_legado_id  TO disciplina_id;
-ALTER TABLE item 
-RENAME column matriz_legado_id  TO matriz_id;
+DO $$
+BEGIN
+  IF EXISTS(SELECT *
+    FROM information_schema.columns
+    WHERE table_name='item' and column_name='are_conhecimento_legado_id')
+  THEN
+      ALTER TABLE "public"."item" RENAME COLUMN "are_conhecimento_legado_id" TO "area_conhecimento_id";
+  END IF;
+end $$ ;
+
+
+DO $$
+BEGIN
+  IF EXISTS(SELECT *
+    FROM information_schema.columns
+    WHERE table_name='item' and column_name='disciplina_legado_id')
+  THEN
+      ALTER TABLE "public"."item" RENAME COLUMN "disciplina_legado_id" TO "disciplina_id";
+  END IF;
+end $$ ;
+
+
+DO $$
+BEGIN
+  IF EXISTS(SELECT *
+    FROM information_schema.columns
+    WHERE table_name='item' and column_name='disciplina_legado_id')
+  THEN
+      ALTER TABLE "public"."item" RENAME COLUMN "matriz_legado_id" TO "matriz_id";
+  END IF;
+end $$ ;
+
