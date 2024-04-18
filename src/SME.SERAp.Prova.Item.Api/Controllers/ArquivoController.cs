73d0f9a6ef89b015f7ec005f7a931ca33c3fc3c0
fernando.groeler <fernando.groeler@gmail.com>
2024-04-18 09:29:20 -0400

    Implementando upload dos arquivos.

---
+++ src/SME.SERAp.Prova.Item.Api/Controllers/ArquivoController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SME.SERAp.Prova.Item.Api.Filters;
+using SME.SERAp.Prova.Item.Aplicacao.Interfaces;
+using SME.SERAp.Prova.Item.Infra.Dtos;
+
+namespace SME.SERAp.Prova.Item.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/Arquivo")]
+    public class ArquivoController : ControllerBase
+    {
+        [ValidaDto]
+        [ProducesResponseType(typeof(RetornoUploadArquivoDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(RetornoBaseDto), StatusCodes.Status500InternalServerError)]
+        [HttpPost("upload", Name = nameof(UploadAsync))]
+        public async Task<IActionResult> UploadAsync([FromBody] UploadArquivoDto request, 
+            [FromServices] IUploadArquivoUseCase useCase)
+        {
+            return Ok(await useCase.ExecutarAsync(request));
+        }
+    }
+}
---
+++ src/SME.SERAp.Prova.Item.Aplicacao/Commands/Arquivo/UploadArquivo/UploadArquivoCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Item.Infra.Dtos;
+
+namespace SME.SERAp.Prova.Item.Aplicacao
+{
+    public class UploadArquivoCommand : IRequest<RetornoUploadArquivoDto>
+    {
+        public UploadArquivoCommand(UploadArquivoDto uploadArquivo)
+        {
+            UploadArquivo = uploadArquivo;
+        }
+
+        public UploadArquivoDto UploadArquivo { get; }
+    }
+}
