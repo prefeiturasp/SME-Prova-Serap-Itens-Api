995c447ba79dd82432e4c3fb474dcd1a810e9c9f
Caíque Siqueira <caique.siqueira@amcom.com.br>
2024-04-19 14:06:48 -0300

    Salvar item command

--- src/SME.SERAp.Prova.Item.Aplicacao/Commands/ItemAudio/Inserir/SalvarItemAudioCommand.cs
+++ src/SME.SERAp.Prova.Item.Aplicacao/Commands/ItemAudio/Inserir/SalvarItemAudioCommand.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SME.SERAp.Prova.Item.Aplicacao.Commands.ItemAudio.Inserir
+namespace SME.SERAp.Prova.Item.Aplicacao.Commands
 {
-    internal class SalvarItemAudioCommand
+    public class SalvarItemAudioCommand : IRequest<long>
     {
+        public SalvarItemAudioCommand(Dominio.Entities.ItemAudio itemAudio)
+        {
+            ItemAudio = itemAudio;
+        }
+        public Dominio.Entities.ItemAudio ItemAudio { get; set; }
     }
 }
